5615c9d6cd970c35ca2a8dffafc8c1cc5c3b3040
Nicholas Trierweiler <nicholastrierweiler@Nicholass-MacBook-Pro.local>
2023-07-26 06:44:31 -0600

    add log

--- src/pages/tools/MathTeacherApp.tsx
+++ src/pages/tools/MathTeacherApp.tsx
@@ -75,6 +75,7 @@ const MathTeacherApp: React.FC = () => {
 
     useEffect(() => {
         if (data) {
+            console.log(data)
             setMarkdown(data.response)
         }
     }, [data])
