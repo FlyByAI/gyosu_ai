8e7617eb39ca1f6aeca7cd61bc1c57c4076fa0e8
Nicholas Trierweiler <nicholastrierweiler@Nicholass-MacBook-Pro.local>
2023-08-03 09:10:48 -0600

    don't let user click when they have trial

--- src/components/TrialButton.tsx
+++ src/components/TrialButton.tsx
@@ -41,7 +41,7 @@ const TrialButton: React.FC<TrialButtonProps> = ({ tokens }) => {
     const { subscriptionInfo, isLoading } = useFetchSubscriptionInfo(`${import.meta.env.VITE_API_URL || notSecretConstants.djangoApi}/user_data/get_subscription_info/`)
     return (!isLoading ?
         <div
-            onClick={activateTrial}
+            onClick={!subscriptionInfo?.active_trial || trialActivated ? activateTrial : () => console.log("Trial already activated")}
             className="font-bold fixed bottom-4 left-4 bg-gradient-to-r from-blue-500 to-green-500 hover:from-blue-600 hover:to-green-600 text-white text-sm rounded-lg h-10 w-32 flex items-center justify-center shadow-2xl cursor-pointer transition-colors z-50">
             <TokenIcon />{trialActivated || subscriptionInfo?.active_trial ? <p className='ms-2'>Trial Active</p> : <p className='ms-2'>Free Trial</p>}
         </div> : null
