f9e8523ae69109402cb7caf2ed025a5efba511c5
Nicholas Trierweiler <nicholastrierweiler@Nicholass-MacBook-Pro-2.local>
2023-10-28 12:39:10 -0600

    add error, and height styles for fit

--- src/components/ContentPlaygroundStreamed.tsx
+++ src/components/ContentPlaygroundStreamed.tsx
@@ -33,6 +33,8 @@ const ContentPlayGroundStreamed = () => {
 
     const playgroundEndpoint = `${apiUrl}/math_app/playground/stream/`;
 
+    const [formError, setFormError] = useState<string | null>(null);
+
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setFormData(prevState => ({
@@ -43,10 +45,17 @@ const ContentPlayGroundStreamed = () => {
 
 
     const handleSubmit = (startStreaming: (bodyContent: any) => void) => {
-        console.log("hand submit")
+        if (formData.problemBankId === null || formData.problemBankId === undefined) {
+            setFormError('Please select a Problem Bank before submitting.');
+            return; // Prevent form from being submitted
+        }
+
+        setFormError(null); // Clear any previous error
+        console.log("hand submit");
         startStreaming(formData);
-        console.log("submit playground")
+        console.log("submit playground");
     };
+
 
     const handleChangeTextbook = (e: ChangeEvent<HTMLSelectElement>) => {
         const { value } = e.target;
@@ -62,29 +71,12 @@ const ContentPlayGroundStreamed = () => {
 
     return (
         <div className="flex justify-center mt-8" style={{ height: 'calc(100vh - 140px)' }}>
-            <div className="w-full flex space-x-10 mx-auto justify-center h-32">
+            <div className="w-full flex space-x-10 mx-auto justify-center">
 
                 {/* Left Form Card */}
-                <div className="relative w-2/5 bg-white p-6 rounded-md shadow-md overflow-y-scroll" style={{ maxHeight: 'calc(100vh - 60px)' }}>
+                <div className="relative w-2/5 bg-white p-6 rounded-md shadow-md overflow-y-scroll h-fit" style={{ maxHeight: 'calc(100vh - 60px)' }}>
 
 
-                    {/* 
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Problem Selection:
-                        </label>
-                        <select
-                            name="useExistingBank"
-                            value={formData.useExistingBank.toString()}
-                            onChange={handleChange} // Using the same handler for all form fields
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        >
-                            <option value="true">Use Existing Problem Bank</option>
-                            <option value="false">Select Problems Automatically</option>
-                        </select>
-                    </div>
-
-                    {formData.useExistingBank ? ( */}
 
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="problemBankId">
@@ -102,100 +94,18 @@ const ContentPlayGroundStreamed = () => {
                                 <option value={doc.id} key={doc.id}>{doc.title}</option>
                             ))}
                         </select>
+
                     </div>
-
-                    {/* ) : (
-                        <>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="textbook">
-                                    Textbook:
-                                </label>
-                                <select
-                                    id="textbook"
-                                    name="textbook"
-                                    value={formData.textbook}
-                                    onChange={handleChangeTextbook}
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                >
-                                    {sourceMaterialOptions.map((option) => (
-                                        <option value={option.value} key={option.value}>
-                                            {option.label}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="teachingTarget">
-                                    Teaching Target:
-                                </label>
-                                <input
-                                    type="text"
-                                    id="teachingTarget"
-                                    name="teachingTarget"
-                                    value={formData.teachingTarget}
-                                    onChange={handleChange}
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    placeholder="What topic are you trying to teach?"
-                                />
-                            </div>
-                        </>
-                    )} */}
-
-                    {/* <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contentType">
-                            Content Type:
-                        </label>
-                        <input
-                            type="text"
-                            id="contentType"
-                            name="contentType"
-                            value={formData.contentType}
-                            onChange={handleChange}
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="Worksheet, Class Activity, Quiz..."
-                        />
-                    </div> */}
-
-                    {/* <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contentLength">
-                            Content Length:
-                        </label>
-                        <input
-                            type="text"
-                            id="contentLength"
-                            name="contentLength"
-                            value={formData.contentLength}
-                            onChange={handleChange}
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="1 page, 1 paragraph, etc."
-                        />
-                    </div> */}
-
-                    {/* <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="additionalDetails">
-                            Additional Details:
-                        </label>
-                        <textarea
-                            id="additionalDetails"
-                            name="additionalDetails"
-                            value={formData.additionalDetails}
-                            onChange={handleChange}
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="Any additional details for the AI"
-                        />
-                    </div> */}
-
-                    {/* <button
-                        onClick={() => handleSubmit}
-                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Submit
-                    </button> */}
-
+                    {formError && <p className="bg-white text-red-500 text-xs italic mt-2 my-2">{formError}</p>}
 
                 </div>
 
+
+
+
                 {/* Right Output Card */}
                 <div className="w-2/5 bg-white p-6 rounded-md shadow-md h-fit">
+
                     <StreamedResponseComponent endpoint={playgroundEndpoint} onSubmit={handleSubmit} />
                 </div>
 
