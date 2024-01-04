d3196057e49743f98707567b679c507e8e8027c2
Nicholas Trierweiler <nicholastrierweiler@Nicholass-MacBook-Pro-2.local>
2024-01-04 06:58:54 -0700

    add bearer token

--- src/hooks/useChat.tsx
+++ src/hooks/useChat.tsx
@@ -1,15 +1,26 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import { useClerk } from '@clerk/clerk-react';  
 import { IChatMessage } from '../pages/GyosuAIChat';
 
 const useChat = (endpoint: string, sessionId?: string) => {
     const queryClient = useQueryClient();
+    const { session } = useClerk(); 
+
+    const getBearerToken = async () => {
+        return session ? `Bearer ${await session.getToken()}` : 'none';
+    };
 
     // Fetch chat data
     const { data: chatData, isLoading, isError, error } = useQuery(['chat', sessionId], async () => {
         // Only fetch if a sessionId is provided
         if (!sessionId) return;
-        const response = await axios.get(`${endpoint}/${sessionId}/`);
+        const bearerToken = await getBearerToken();
+        const response = await axios.get(`${endpoint}/${sessionId}/`, {
+            headers: {
+                'Authorization': bearerToken,
+            },
+        });
         return response.data;
     }, {
         enabled: !!sessionId  // Only run query if sessionId is available
@@ -17,12 +28,17 @@ const useChat = (endpoint: string, sessionId?: string) => {
 
     // Send message mutation
     const sendMessageMutation = useMutation(async ({ newMessage, allMessages }: { newMessage: IChatMessage, allMessages: IChatMessage[] }) => {
+        const bearerToken = await getBearerToken();
         const payload = {
             newMessage,
             conversation: allMessages,
         };
         const url = sessionId ? `${endpoint}/${sessionId}/` : endpoint;
-        const response = await axios.post(url, payload);
+        const response = await axios.post(url, payload, {
+            headers: {
+                'Authorization': bearerToken,
+            },
+        });
         return response.data;
     }, {
         onSuccess: (data) => {
