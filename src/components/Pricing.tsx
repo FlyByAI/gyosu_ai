b6cd911c11b410847332919ffc49572965068815
dustycyanide <dustycyanide13@gmail.com>
2024-01-23 17:47:22 -0700

    update message limit for paid tier to 200 per day

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -37,7 +37,7 @@ const Pricing = () => {
                         {/* <p className='my-4 text-left'>Plan includes:</p> */}
                         <ul className='list-decimal-none list-inside text-left space-y-4 mt-4 flex flex-col'>
                             <li className="flex flex-row"><CheckmarkIcon /> GyosuChat Assistant</li>
-                            <li className="flex flex-row"><CheckmarkIcon /> 25 message limit every 3 hours </li>
+                            <li className="flex flex-row"><CheckmarkIcon /> 200 message limit per day. </li>
                             <li className="flex flex-row"><CheckmarkIcon />Full access to problem database</li>
                             <li className="flex flex-row"><CheckmarkIcon />Early Access to additional features</li>
                         </ul>
