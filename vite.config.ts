e7145ef2f6c37d7af887cc271b8edb5af2361579
Nick Trierweiler <nitr8087@colorado.edu>
2024-02-02 18:06:33 +0200

    Revert "add vite history plugin"

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import history from 'vite-plugin-history-api-fallback';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
+  resolve: {
     alias: {
       process: "process/browser",
       stream: "stream-browserify",
@@ -15,8 +14,7 @@ export default defineConfig({
   define: {
     'process.env': process.env
   },
-plugins: [
+  plugins: [
     react(),
-    history(),
   ]
 })
