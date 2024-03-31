d483779fcae402eb008ef4df562a98a1a33081e8
kicksent <wingman@flybyai.io>
2024-03-31 09:27:35 -0600

    fix padding, margin, and dark bg showing on prob search page

--- src/components/navbars/FixedNavbar.tsx
+++ src/components/navbars/FixedNavbar.tsx
@@ -14,7 +14,7 @@ const FixedNavbar: React.FC = () => {
 
   return (
     <>
-      <div className='mt-20 md:mt-16'></div>
+      <div className='mt-16'></div>
       <header className="navbar fixed top-0 left-0 z-20 w-full bg-base-100">
         {/* Mobile view */}
         <div className="navbar-start md:hidden">
